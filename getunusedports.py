2431b2e8f0c282a8a8c97019737d0eae0f2bccf2
narrowtomato <aaronhahn6@gmail.com>
2020-11-14 19:23:03 -0600

    Add files via upload

--- getunusedports.py
+++ getunusedports.py
@@ -8,32 +8,36 @@ ip = input("Enter device IP/FQDN: ")
 username = input("user: ")
 passwd = getpass.getpass("pass: ")
 
-#Connect to the switch
-device = netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=passwd)
+try:
+    #Connect to the switch
+    device = netmiko.ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=passwd)
 
-#Write message stating successful connection
-print("Connected successfully to " + ip )
+    #Write message stating successful connection
+    print("Connected successfully to " + ip )
 
-#Get show interfaces output
-showintoutput = device.send_command("show interfaces")
+    #Get show interfaces output
+    showintoutput = device.send_command("show interfaces")
 
-#Disconnect from the switch
-device.disconnect()
+    #Disconnect from the switch
+    device.disconnect()
 
-interfaceline = ""
-lastusedline = ""
-finaloutput = ""
-numfreeports = 0
+    interfaceline = ""
+    lastusedline = ""
+    finaloutput = ""
+    numfreeports = 0
 
-#For each line in the command output
-for line in showintoutput.splitlines():
-    if "protocol" in line:
-        interfaceline = line
-    if "Last input" in line:
-        lastusedline = line
-        if ('y' in lastusedline or ("input never" in lastusedline and "output never" in lastusedline)) and not (':' in lastusedline):
-            finaloutput += interfaceline + "\n" + lastusedline + "\n"
-            numfreeports += 1
+    #For each line in the command output
+    for line in showintoutput.splitlines():
+        if "protocol" in line:
+            interfaceline = line
+        if "Last input" in line:
+            lastusedline = line
+            if ('y' in lastusedline or ("input never" in lastusedline and "output never" in lastusedline)) and not (':' in lastusedline):
+                finaloutput += interfaceline + "\n" + lastusedline + "\n"
+                numfreeports += 1
 
-print("\nPorts not used in over a year:  " + str(numfreeports) + "\n\n")
-print(finaloutput)
+    print("\nPorts not used in over a year:  " + str(numfreeports) + "\n\n")
+    print(finaloutput)
+
+except:
+    print("Could not connect to the device")
